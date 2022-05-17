52dc2c401faf22c557f43e1fd0449ad2b438c6d4
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-05-17 23:34:34 +0530

    added header component and implemented light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useState } from "react";
+
+import Header from "./components/Header";
 
 function App() {
+  const [mode, setMode] = useState("dark");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${mode}`}>
+      <Header mode={mode} setMode={setMode} />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button, Container } from "reactstrap";
+const Header = ({ mode, setMode }) => {
+  return (
+    <Container className={"p-4"} fluid>
+      <div className="toggle-btn">
+        <Button
+          onClick={() => setMode(mode === "dark" ? "light" : "dark")}
+          className={`btn shadow-none btn-outline-${mode}`}
+        >
+          {mode === "dark" ? "☀️ Light" : "🌙 Dark"}
+        </Button>
+      </div>
+      <h1 className={`pt-5 ${mode}`}>Our Menu</h1>
+    </Container>
+  );
+};
+
+export default Header;
