d0b3b49d87eca14679aeeb534b4c255408a23e62
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-06-06 10:59:50 +0530

    fixed images links

--- src/data/dishes.js
+++ src/data/dishes.js
@@ -1,58 +1,58 @@
 export default [
-  {
-    title: "Apple Pie",
-    image:
-      "https://imgs.search.brave.com/_oie9VBkIRosPzApMf2KwqjCbNITbg47GGgq6KPNkE8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pbWFn/ZXMuY3RmYXNzZXRz/Lm5ldC8zczVpbzZt/bnhmcXovQ05YejZZ/R25TTzdKd0VoaXZ2/eTdVLzI5NTMwZTAw/NTg2MTIzZWNiNTM3/ZWI5NjZlYzI5ZjQy/L2ltYWdlMS5qcGc",
-    subtext: "Lorem ipsum dolor sit amet consectetur.",
-    price: "₹ 680",
-    type: "sweet",
-  },
-  {
-    title: "Apple Chips",
-    image:
-      "https://imgs.search.brave.com/8iyWrx7eLJ6ipPQgl88a3q9KIutHuduulBgBLqHkLRo/rs:fit:1080:1080:1/g:ce/aHR0cHM6Ly93d3cu/Y3Jpc3B5Zm9vZGlk/ZWEuY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDIxLzA1L0Jh/a2VkLUFwcGxlLUNo/aXBzLmpwZw",
-    subtext: "Lorem ipsum dolor sit amet consectetur.",
-    price: "₹ 300",
-    type: "snack",
-  },
-  {
-    title: "Apple",
-    image:
-      "https://imgs.search.brave.com/mkkuIXMjVhIcFLnjkBHVhn_gSYjl3NrJS8OEmmKoaPo/rs:fit:1200:1078:1/g:ce/aHR0cHM6Ly9taXIt/czMtY2RuLWNmLmJl/aGFuY2UubmV0L3By/b2plY3RfbW9kdWxl/cy9tYXhfMTIwMC80/YjY4NjI0MzA0OTQ4/OS41ODFmMGUxNzBl/NmM4LmpwZw",
-    subtext: "Lorem ipsum dolor sit amet consectetur.",
-    price: "Priceless!",
-    type: "snack",
-  },
-  {
-    title: "Apple Dumplings",
-    image:
-      "https://imgs.search.brave.com/sH9WTt3ViO3VwuEYfnvO5vTSDdW7QcHuFj7ue3g2nMo/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pbWFn/ZXN2Yy5tZXJlZGl0/aGNvcnAuaW8vdjMv/bW0vaW1hZ2U_dXJs/PWh0dHBzOiUyRiUy/RnN0YXRpYy5vbmVj/bXMuaW8lMkZ3cC1j/b250ZW50JTJGdXBs/b2FkcyUyRnNpdGVz/JTJGMjQlMkYyMDE1/JTJGMTAlMkZiYWtl/ZC1hcHBsZS1kdW1w/bGluZ3MtMjAuanBn",
-    subtext: "Lorem ipsum dolor sit amet consectetur.",
-    price: "₹ 500",
-    type: "sweet",
-  },
-  {
-    title: "Applesauce Cake",
-    image:
-      "https://imgs.search.brave.com/ZAFUwDzXtSID-3D_SOqy4AfA4nz-fVpYByKYXKe8EJ8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9ibHVl/Y2F5ZW5uZS5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMTkv/MDIvRFNDXzExNTcu/anBn",
-    subtext: "Lorem ipsum dolor sit amet consectetur.",
-    price: "₹ 900",
-    type: "sweet",
-  },
-  {
-    title: "Caramel Apple",
-    image:
-      "https://imgs.search.brave.com/zju4gY-ms1wjZDQATpUaJyp2GHTt0m64i62A2XqKaB8/rs:fit:1200:1200:1/g:ce/aHR0cDovL3NpbXBs/eWNvb2tpbmcxMDEu/Y29tL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDE0LzEwL2NhcmFt/ZWwtYXBwbGVzLXNx/dWFyZS5qcGc_dz0z/MDA",
-    subtext: "Lorem ipsum dolor sit amet consectetur.",
-    price: "₹ 400",
-    type: "sweet",
-  },
-  {
-    title: "Baked Apple",
-    image:
-      "https://imgs.search.brave.com/BGzcNJmtR-DfbKxrqG7C1sWLrVJvXbLdw4O09IHv8zw/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly90aGVj/bGVhbmVhdGluZ2Nv/dXBsZS5jb20vd3At/Y29udGVudC91cGxv/YWRzLzIwMTkvMDkv/aGVhbHRoeS1iYWtl/ZC1hcHBsZXMtMS5q/cGc",
-    subtext: "Lorem ipsum dolor sit amet consectetur.",
-    price: "₹ 800",
-    type: "snack",
-  },
+	{
+		title: "Apple Pie",
+		image:
+			"https://images.unsplash.com/photo-1572383672419-ab35444a6934?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+		subtext: "Lorem ipsum dolor sit amet consectetur.",
+		price: "₹ 680",
+		type: "sweet",
+	},
+	{
+		title: "Apple Chips",
+		image:
+			"https://images.pexels.com/photos/8963959/pexels-photo-8963959.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		subtext: "Lorem ipsum dolor sit amet consectetur.",
+		price: "₹ 300",
+		type: "snack",
+	},
+	{
+		title: "Apple",
+		image:
+			"https://images.pexels.com/photos/693794/pexels-photo-693794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		subtext: "Lorem ipsum dolor sit amet consectetur.",
+		price: "Priceless!",
+		type: "snack",
+	},
+	{
+		title: "Apple Dumplings",
+		image:
+			"https://www.31daily.com/wp-content/uploads/2018/10/md_Apple-Dumplings-with-Maple-Caramel-Sauce-2-1-of-1.jpg",
+		subtext: "Lorem ipsum dolor sit amet consectetur.",
+		price: "₹ 500",
+		type: "sweet",
+	},
+	{
+		title: "Applesauce Cake",
+		image:
+			"https://butterwithasideofbread.com/wp-content/uploads/2020/08/Spiced-Applesauce-Cake.3.jpg",
+		subtext: "Lorem ipsum dolor sit amet consectetur.",
+		price: "₹ 900",
+		type: "sweet",
+	},
+	{
+		title: "Caramel Apple",
+		image:
+			"https://images.unsplash.com/photo-1594149433274-005eb6420335?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+		subtext: "Lorem ipsum dolor sit amet consectetur.",
+		price: "₹ 400",
+		type: "sweet",
+	},
+	{
+		title: "Baked Apple",
+		image:
+			"https://images.pexels.com/photos/6210952/pexels-photo-6210952.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		subtext: "Lorem ipsum dolor sit amet consectetur.",
+		price: "₹ 800",
+		type: "snack",
+	},
 ];
